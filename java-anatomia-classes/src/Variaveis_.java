6c3de0f3d43b5b1eac72f6e36670b0961c77334b
Rafael <rafaelssilva65@hotmail.com>
2023-04-13 08:13:06 -0300

    constantes

---
+++ java-anatomia-classes/src/Variaveis_.java
@@ -0,0 +1,10 @@
+public class Variaveis_ {
+     
+public static void main(String[] args){
+
+    final double VALOR_DE_PI = 3.14; //Constante em java, deve ser declarada em caixa alta.
+
+    System.out.println(VALOR_DE_PI);
+
+    }
+}
